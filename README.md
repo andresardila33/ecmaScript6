69f42a1f68d8a7f73d7ccc4eac0c275f6f63cb80
Andres Ardila <andresardila33@gmail.com>
2022-11-07 18:47:14 -0500

    feat: setting up the environment

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ecmaScript6
+<p align="center">
+  <a href="https://es.wikipedia.org/" target="blank"><img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" width="160" alt="160" /></a>
+</p>
+
+# EcmaScript6
+
 Curso de ECMASCRIPT6+ de Platzi
---
+++ src/es6/00-let-const.js
@@ -0,0 +1,23 @@
+/*var lastName = 'David';
+lastName = 'Oscar';
+console.log(lastName);
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+const animal = 'Dog';
+animal = 'Cat';*/
+
+const fruits = () => {
+    if(true) {
+    var fruit1 = 'Apple'; //Function scope
+    let fruit2 = 'Kiwi'; // Block scope
+    const fruit3 = 'Banana'; // Block scope
+    }
+    console.log(fruit1);
+    //console.log(fruit2);
+    //console.log(fruit3);    
+}
+fruits();
+
