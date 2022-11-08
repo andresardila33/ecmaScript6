02b420044863ab6f510acd4ea7a92bd5c6baa875
Andres Ardila <andresardila33@gmail.com>
2022-11-07 20:37:05 -0500

    feat: adding the file 02-strings.js

---
+++ src/es6/02-strings.js
@@ -0,0 +1,12 @@
+let hello = 'Hello';
+let world = 'World';
+
+// Template literals
+const epicPhrase = `${hello} ${world}!!`
+console.log(epicPhrase);
+
+// Multi-line strings
+let lorem = `Esto es una linea
+esto es otra linea
+y esto tambien es otra linea`;
+console.log(lorem);
